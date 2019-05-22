59b2adbaef20a07c58fe7e41a9793a504c5496fd
amalthomas111 <amalthomas111@gmail.com>
2019-05-22 13:27:06 -0700

    fixed bug

--- scripts/getgsedetails_srrorsrplist.sh
+++ scripts/getgsedetails_srrorsrplist.sh
@@ -2,25 +2,19 @@
 # Author: A.T
 # Need efetch/esearch from entrez-direct. Install via conda
 
-set -euo pipefail
+#set -euo pipefail
 if [ "$#" -ne 3 ]; then
 echo "bash script.sh srr/srp file(one srr/srp per line) SRR/SRP outputname"
 echo "bash script.sh srrid.txt SRR myoutput"
 exit
 fi
 
-rscriptpath=get_srr-srp-gse_details_tab.R
 # check for input files
 if [ ! -f $1 ];then
         echo "Inputfile:${1} not found! Exiting"
         exit
 fi
 
-if [ ! -f ${rscriptpath} ]; then
-        echo "Rscript ${rscriptpath} not found!"
-        echo "Needs ${rscriptpath} in path! Exiting!"
-        exit
-fi
 
 id=$(echo "$2" | awk '{print tolower($0)}')
 #echo "id:${id}"
@@ -41,7 +35,20 @@ tempdir=$(mktemp -d)
 echo "tempdir:${tempdir}"
 cd $tempdir
 split -l 1  ${path}/${1}.temp
-ls -1 x*|while read i;do cat $i|while read j;do srr=$j; esearch -db sra -query $srr|efetch -format runinfo|sed '1d'|grep $srr |cut -d',' -f 1,30,21|sed '/^$/d'|tr "," "\t"  >> ${path}/${3}.srr_srp_gsmid.txt;done;done
-rm -rf ${1}.temp
+echo "Finding SRR for each SRPs"
+ls -1 x*|while read i;do
+        cat $i|while read j;do
+                srr=$j
+                esearch -db sra -query $srr|efetch -format runinfo|sed '1d'|grep $srr |cut -d',' -f 1,30,21|sed '/^$/d'|tr "," "\t"
+                done
+        done >  ${path}/${3}.srr_srp_gsmid.txt
+rm -rf ${path}/${1}.temp
+echo "SRR SRP GSM list created"
+rscriptpath=get_srr-srp-gse_details_tab.R
+if [ ! -f ${rscriptpath} ]; then
+        echo "Rscript ${rscriptpath} not found!"
+        echo "Needs ${rscriptpath} in path! Exiting!"
+        exit
+fi
 cd ${path}
 Rscript ${rscriptpath} ${3}.srr_srp_gsmid.txt $3
